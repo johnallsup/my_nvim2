c1e549c054c892a84e9aa16dc500cd1ecbaed2dd
John Allsup <dev@chalisque.net>
2024-01-23 17:36:21 +0000

    Added mode dependent line numbers

--- lua/jda/styles.lua
+++ lua/jda/styles.lua
@@ -1,11 +1,12 @@
- -- change background depending on mode
- vim.api.nvim_create_autocmd({ "InsertEnter" }, {
-	callback = function()
-		vim.api.nvim_set_hl(0, "Normal", {bg="#330000"})
-	end
+vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+    callback = function()
+        vim.api.nvim_set_hl(0, "Normal", {bg="#330000"})
+        vim.api.nvim_set_hl(0, "LineNr", {bg="#770000"})
+    end
 })
     vim.api.nvim_create_autocmd({ "InsertLeave" }, {
-	callback = function()
-		vim.api.nvim_set_hl(0, "Normal", {bg="#000000"})
-	end
+    callback = function()
+        vim.api.nvim_set_hl(0, "Normal", {bg="#000000"})
+        vim.api.nvim_set_hl(0, "LineNr", {bg="#000077"})
+    end
 })
