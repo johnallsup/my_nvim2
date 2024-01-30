4cc5fdbc9a92346bac84296e7dc6945072bfa8a6
John Allsup <dev@chalisque.net>
2024-01-30 13:53:14 +0000

    Added simple osc examples

---
+++ lua/jda/osc.lua
@@ -0,0 +1,17 @@
+local osc = require'osc'.new{
+  transport = 'udp',
+  recvAddr = '0.0.0.0',
+  recvPort  = 8999,
+}
+
+-- register a "catch all" handler
+-- osc:add_handler('*', function(data)
+--   print(vim.inspect(data))
+-- end)
+osc:add_handler("/bobbins", vim.schedule_wrap(function(data)
+    vim.cmd('%s/$/hello')
+  end)
+)
+
+-- open the OSC server
+osc:open()
